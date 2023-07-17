138a817b76aa9fd8f7880937c9c38249d3ad31ed
burnjoe <sabanajholo@gmail.com>
2023-07-17 16:36:28 +0800

    installed + configured Tailwind CLI

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// here you can edit the tailwind.config.js to customize your configuration with your own design tokens
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],          // look for any html files in root folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
